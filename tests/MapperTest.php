3b760c2a5ea898c3da31e1e1098cc49eda67ac4b
Dennis Snijder <dennissnijder97@gmail.com>
2017-05-16 16:44:04 +0200

    Added a simple test to test the Mapper with a simple scenario.

---
+++ tests/MapperTest.php
@@ -0,0 +1,70 @@
+<?php
+
+use Enrise\Maparea\Mapper;
+use PHPUnit\Framework\TestCase;
+
+
+class MapperTest extends TestCase
+{
+    /**
+     * @var Mapper
+     */
+    private $mapper;
+
+    protected function setUp()
+    {
+        $this->mapper = new Mapper();
+    }
+
+    /**
+     * Tests simple mapping of data.
+     */
+    public function testSimpleArrayMapping()
+    {
+        $mappedData = $this->mapper->mapData(
+            $this->getRawData(),
+            $this->getMappingConfig()
+        );
+
+
+        $this->assertSame($mappedData, $this->getDesiredOutput());
+    }
+
+    /**
+     * @return array
+     */
+    private function getMappingConfig(): array
+    {
+        return [
+            "id" => [
+                "from" => "uuid"
+            ],
+            "name" => [
+                "from" => "customer_name"
+            ]
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getRawData(): array
+    {
+        return [
+            "uuid" => 1,
+            "customer_name" => "John doe"
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getDesiredOutput(): array
+    {
+        return [
+            "id" => 1,
+            "name" => "John doe"
+        ];
+    }
+
+}
